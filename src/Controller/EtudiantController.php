e08cc2d95b1e34c1b4944caa7607cf7c618dfd9a
medlasswed <mohamed.lasswed2001@gmail.com>
2022-05-15 08:07:27 +0100

    examen tp

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -3,50 +3,103 @@
 namespace App\Controller;
 
 use App\Entity\Etudiant;
-use App\Entity\Personne;
 use App\Form\EtudiantType;
-use App\Form\PersonneType;
-use App\Repository\EtudiantRepository;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[
+    Route('etudiants')
+]
 class EtudiantController extends AbstractController
 {
     #[Route('/etudiant', name: 'app_etudiant')]
-    public function index(EtudiantRepository $EtudiantRepository): Response
+    public function index(): Response
     {
-        $Etudiants = $EtudiantRepository->findAll();
-        return $this->render('etudiant/index.html.twig', ['etudiants' => $Etudiants]);
+        return $this->render('etudiant/index.html.twig', [
+            'controller_name' => 'EtudiantController',
+        ]);
     }
 
-    #[Route('/etudiant/add', name: 'app_etudiant_add')]
-    public function addEtudiant(EtudiantRepository $userRepository, Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $etudiant = new Etudiant();
-        $form = $this->createForm(EtudiantType::class, $etudiant);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($etudiant);
-            $entityManager->flush();
-            $this->addFlash("success", "etudiant ajouté avec success");
-            return $this->redirectToRoute('app_etudiant');
-        }
-        return $this->render('etudiant/add.html.twig', ['form' => $form->createView()]);
+    #[
+        Route('/{page<\d+>?1}/{nbre<\d+>?12}', name: 'etudiant.list')
+    ]
+    public function indexAlls(ManagerRegistry $doctrine, $page, $nbre): Response {
+        $repository = $doctrine->getRepository(Etudiant::class);
+        $nbPersonne = $repository->count([]);
+        // 24
+        $nbrePage = ceil($nbPersonne / $nbre) ;
+
+        $etudiants = $repository->findBy([], [],$nbre, ($page - 1 ) * $nbre);
+
+        return $this->render('etudiant/index.html.twig', [
+            'etudiants' => $etudiants,
+            'isPaginated' => true,
+            'nbrePage' => $nbrePage,
+            'page' => $page,
+            'nbre' => $nbre
+        ]);
     }
-    #[Route('/etudiant/delete/{id}', name: 'app_etudiant_delete')]
-    public function deleteEtudiant($id, EtudiantRepository $repository, Etudiant $userObj = null, EntityManagerInterface $entityManager, ManagerRegistry $doctrine): Response
+
+    #[Route('/edit/{id?0}', name: 'etudiant.edit')]
+    public function addPersonne(Etudiant $etudiant = null, ManagerRegistry $doctrine, Request $request): Response
     {
-        $entityManager = $doctrine->getManager();
-        $etudiant = $repository->findOneBy(['id' => $id]);
-        if (isset($etudiant)) {
-            $entityManager->remove($etudiant);
-            $entityManager->flush();
+        $new = false;
+        //$this->getDoctrine() : Version Sf <= 5
+        if (!$etudiant) {
+            $new = true;
+            $etudiant = new Etudiant();
         }
-        return $this->redirectToRoute('app_etudiant');
+
+        // $etudiant est l'image de notre formulaire
+        $form = $this->createForm(EtudiantType::class, $etudiant);
+        // Mn formulaire va aller traiter la requete
+        $form->handleRequest($request);
+        //Est ce que le formulaire a été soumis
+        if($form->isSubmitted()) {
+            // si oui,
+            // on va ajouter l'objet Etudiant dans la base de données
+            $manager = $doctrine->getManager();
+            $manager->persist($etudiant);
+
+            $manager->flush();
+            // Afficher un mssage de succès
+            if($new) {
+                $message = " a été ajouté avec succès";
+            } else {
+                $message = " a été mis à jour avec succès";
+            }
+            $this->addFlash('success',$etudiant->getNom(). $message );
+            // Rediriger verts la liste des etudiant
+            return $this->redirectToRoute('etudiant.list');
+        } else {
+            //Sinon
+            //On affiche notre formulaire
+            return $this->render('etudiant/add.html.twig', [
+                'form' => $form->createView()
+            ]);
+        }
+
+    }
+
+    #[Route('/delete/{id}', name: 'etudiant.delete')]
+    public function deletePersonne(Etudiant $etudiant = null, ManagerRegistry $doctrine): RedirectResponse {
+        // Récupérer la etudiant
+        if ($etudiant) {
+            // Si la etudiant existe => le supprimer et retourner un flashMessage de succés
+            $manager = $doctrine->getManager();
+            // Ajoute la fonction de suppression dans la transaction
+            $manager->remove($etudiant);
+            // Exécuter la transacition
+            $manager->flush();
+            $this->addFlash('success', "L etudiant a été supprimé avec succès");
+        } else {
+            //Sinon  retourner un flashMessage d'erreur
+            $this->addFlash('error', "etudiant innexistante");
+        }
+        return $this->redirectToRoute('etudiant.list');
     }
 }
---
+++ src/DataFixtures/SectionFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Etudiant;
+use App\Entity\Section;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as FakerFactory;
+
+class SectionFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $faker = FakerFactory::create('fr_FR');
+        for ($i=0; $i < 5; $i++) {
+            $section = new Section();
+            $section->setDesignation($faker->word);
+            for ($j=0; $j < rand(1, 5); $j++) {
+                $etudiant = new Etudiant();
+                $etudiant->setNom($faker->lastName);
+                $etudiant->setPrenom($faker->firstName);
+                $section->addEtudiant($etudiant);
+                $etudiant->setSection($section);
+                $manager->persist($etudiant);
+            }
+            $manager->persist($section);
+        }
+
+        for ($i=0; $i < 5; $i++) {
+            $etudiant = new Etudiant();
+            $etudiant->setNom($faker->lastName);
+            $etudiant->setPrenom($faker->firstName);
+            $manager->persist($etudiant);
+        }
+        $manager->flush();
+    }
+}
